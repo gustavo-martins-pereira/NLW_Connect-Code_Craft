64e26d927692d14d10d7d54b660a51e8e791b35b
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2025-02-22 14:12:08 -0300

    A&M - Main Page Content & Input Rework

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,34 @@
-import { Mail } from "lucide-react";
 import type { ComponentProps } from "react";
 
-interface InputProps extends ComponentProps<"input"> {
+interface InputRootProps extends ComponentProps<"div"> {
     error?: boolean;
-};
+}
 
-export function Input({ error = false, ...props }: InputProps) {
+export function InputRoot({ error = false, ...props }: InputRootProps) {
     return (
         <div
-            className="bg-gray-800 h-12 flex items-center gap-2 border border-gray-800 rounded-xl px-4 group data-[error=true]:border-danger"
             data-error={error}
-        >
-            <span className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger">
-                <Mail />
-            </span>
+            className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger"
+            {...props}
+        />
+    );
+}
 
-            <input
-                className="flex-1 outline-0 placeholder:text-gray-400"
-                {...props}
-            />
-        </div>
+interface InputIconProps extends ComponentProps<"span"> {}
+
+export function InputIcon({ ...props }: InputIconProps) {
+    return (
+        <span
+            className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
+            {...props}
+        />
+    );
+}
+
+interface InputFieldProps extends ComponentProps<"input"> {}
+
+export function InputField({ ...props }: InputFieldProps) {
+    return (
+        <input className="flex-1 outline-0 placeholder-gray-400" {...props} />
     );
 }
